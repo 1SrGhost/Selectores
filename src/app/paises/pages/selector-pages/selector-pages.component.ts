8115a27d66a130838ad071688f277202ef8592a5
Andres Perez <edgar4698@hotmail.com>
2022-10-27 01:14:26 -0500

    purgar el segundo selector cuando el primero cambia

--- src/app/paises/pages/selector-pages/selector-pages.component.ts
+++ src/app/paises/pages/selector-pages/selector-pages.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { PaisSmall } from '../../interfaces/paises.interface';
 import { PaisesServiceService } from '../../services/paises.service';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-selector-pages',
@@ -10,16 +13,30 @@ import { PaisesServiceService } from '../../services/paises.service';
 export class SelectorPagesComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required],
+    pais: ['', Validators.required],
   });
   regiones: string[] = [];
+  paises: PaisSmall[] = [];
   constructor(
     private fb: FormBuilder,
     private paisesServices: PaisesServiceService
   ) {}
 
   ngOnInit(): void {
-    this.regiones = this.paisesServices.regiones;
-    console.log(this.regiones)
+      this.regiones = this.paisesServices.regiones
+
+      // this.miFormulario.get('region')?.valueChanges.subscribe((region) => {
+      //   this.paisesServices
+      //     .getPaisesPorRegion(region)
+      //       .subscribe((paises) => (this.paises = paises));
+      // });
+
+      this.miFormulario.get('region')?.valueChanges.pipe(
+        tap(( _ ) => {this.miFormulario.get('pais')?.reset('');
+      }),
+        switchMap(region =>  this.paisesServices.getPaisesPorRegion(region))
+        
+      ).subscribe(paises => {this.paises= paises;})
   }
 
   guardar() {}
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PaisSmall } from '../interfaces/paises.interface';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesServiceService {
-  private _regiones: string[] = ['Africa', 'Europe', 'America','Asia', 'Oceania'];
+  private _baseUrl:string = 'https://restcountries.com/v2'
+  private _regiones: string[] = ['Africa', 'Europe', 'Americas','Asia', 'Oceania'];
 
   get regiones(): string[]{
     return [...this._regiones];
   }
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+ /**
+  * Toma una cadena como parámetro y devuelve un Observable de tipo any
+  * @param {string} region - La región de la que desea obtener los países.
+  * @returns Un Observable de tipo any.
+  */
+  getPaisesPorRegion(region: string): Observable<PaisSmall[]>{
+    const url: string = `${this._baseUrl}/region/${region}?fields=alpha3Code,name`
+    return this.http.get<PaisSmall[]>(url);
+  }
 }
